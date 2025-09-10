2f98c2857d81a91fdc7c7b5e551a4061ac6629ac
LenaH6 <jaribfernando2001@gmail.com>
2025-09-10 14:00:57 -0500

    Update api-config.js

--- js/api-config.js
+++ js/api-config.js
@@ -1,2 +1,2 @@
-// Centraliza la URL del backend (mismo dominio por defecto)
+// API base del backend (por defecto, mismo dominio)
 window.API_BASE = window.API_BASE || (window.location.origin.replace(/\/$/, ''));
